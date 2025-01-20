8ef76a06de49f204916c1c102da7b379cbf92184
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-01-21 01:08:32 +0600

    Login page styled

--- Nexora/settings.py
+++ Nexora/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     # Tailwind CSS
     'tailwind',
     'theme',
@@ -133,6 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 
 # Tailwind CSS settings
--- users/forms.py
+++ users/forms.py
@@ -11,3 +11,19 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password', 'password2']
+        
+class LoginForm(forms.Form):
+    login = forms.CharField(
+        label="Username",  # Optional label override
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your username',  # Optional placeholder
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+        })
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter your password',
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+        })
+    )
