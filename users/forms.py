1f340eb44d1a67cbd1974c41e3804f0ecb66e408
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-01-23 16:22:11 +0600

    landing page added

--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,37 @@ from django.contrib.auth import get_user_model
 from users.models import User
 
 
+
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter your password',
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+        })
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password',
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+        })
+    )
 
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Enter your username',
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'Enter your email',
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+            })
+        }
+
         
 class LoginForm(forms.Form):
     login = forms.CharField(
